e652cce0e1186fc143f05bdafc783461a10abd38
阿瑜 <acs104135@gm.ntcu.edu.tw>
2019-02-26 18:04:32 +0800

    Update 10986.cpp

--- 10986.cpp
+++ 10986.cpp
@@ -16,52 +16,52 @@ int main()
 	for(int c=1;c<=tc;c++)
 	{
 	  
- 	int n,m,s,t;
- 	scanf("%d %d %d %d",&n,&m,&s,&t);
- 	vector< pair<int,int> > adj[n+1];
+ 		int n,m,s,t;
+ 		scanf("%d %d %d %d",&n,&m,&s,&t);
+ 		vector< pair<int,int> > adj[n+1];
  	
- 	for(int i=0;i<m;i++)
- 	{
- 		int a,b,w;
- 		scanf("%d %d %d",&a,&b,&w);
-		adj[a].push_back({b,w});
-		adj[b].push_back({a,w});	
-	}
-	int dis[n+1];
-	int visited[n+1];
+ 		for(int i=0;i<m;i++)
+ 		{
+ 			int a,b,w;
+ 			scanf("%d %d %d",&a,&b,&w);
+			adj[a].push_back({b,w});
+			adj[b].push_back({a,w});	
+		}
+		int dis[n+1];
+		int visited[n+1];
 	
-	for(int i=0;i<n;i++)
-	{
-		dis[i] = inf ;
-		visited[i] = 0;
-	}
-	dis[s] = 0;
-	visited[s] = 1;
-	priority_queue< pair<int,int> > pq;
-	pq.push({0,s});
-	while(!pq.empty())
-	{
-		int b = pq.top().second;
-		int w = pq.top().first;
-		pq.pop();
-		visited[b]=1;
-		
-		for(int i=0;i<adj[b].size();i++)
+		for(int i=0;i<n;i++)
 		{
-			int tb = adj[b][i].first;
-			int tw = adj[b][i].second;
-			if(dis[tb]>dis[b]+tw && visited[tb]==0)
+			dis[i] = inf ;
+			visited[i] = 0;
+		}
+		dis[s] = 0;
+		visited[s] = 1;
+		priority_queue< pair<int,int> > pq;
+		pq.push({0,s});
+		while(!pq.empty())
+		{
+			int b = pq.top().second;
+			int w = pq.top().first;
+			pq.pop();
+			visited[b]=1;
+		
+			for(int i=0;i<adj[b].size();i++)
 			{
-				dis[tb] = dis[b]+tw;
-				pq.push({-dis[tb],tb});
+				int tb = adj[b][i].first;
+				int tw = adj[b][i].second;
+				if(dis[tb]>dis[b]+tw && visited[tb]==0)
+				{
+					dis[tb] = dis[b]+tw;
+					pq.push({-dis[tb],tb});
+				}
 			}
 		}
-	}
-	cout << "Case #" << c <<": "; 
-	if(dis[t]!=inf)
-		cout << dis[t] << endl;
-	else
-		cout << "unreachable" << endl;	
+		cout << "Case #" << c <<": "; 
+		if(dis[t]!=inf)
+			cout << dis[t] << endl;
+		else
+			cout << "unreachable" << endl;	
 	}
 	return 0;
 } 
